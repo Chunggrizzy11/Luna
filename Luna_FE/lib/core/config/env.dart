57ec2c61cd4f73a6a62674f189a9a996d6606314
Chunggrizzy11 <chungthanhpham2112@gmail.com>
2026-08-06 09:19:53 +0700

    fix

--- Luna_FE/lib/core/config/env.dart
+++ Luna_FE/lib/core/config/env.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/foundation.dart' show kIsWeb, kDebugMode;
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 abstract final class Env {
   static const _apiBaseUrl = String.fromEnvironment(
@@ -8,7 +8,6 @@ abstract final class Env {
 
   static String get apiBaseUrl {
     if (_apiBaseUrl.isNotEmpty) return _apiBaseUrl;
-    if (kDebugMode && kIsWeb) return 'http://localhost:3000/api/v1';
     if (kIsWeb) return 'https://luna-7tkq.onrender.com/api/v1';
     return 'http://10.0.2.2:3000/api/v1';
   }
